68aff8ee268292686af3ea5c37cf1b21805a306b
eynokoushal <eynokoushal@gmail.com>
2024-11-22 11:50:54 +0530

    Created nodeMailer functionality

--- app/helpers/db.js
+++ app/helpers/db.js
@@ -2,4 +2,5 @@ module.exports = {
     User: require('../models/user.model'),
     Courses : require('../models/courses.model'),
     Categories : require('../models/categories.model'),
+    Orders: require('../models/orders.model')
 }
---
+++ app/models/orders.model.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+const mongoosePaginate = require("mongoose-paginate-v2");
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    // userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    companyName: { type: String },
+    country: { type: String, required: true },
+    streetAddress: { type: String, required: false },
+    flat: { type: String },
+    city: { type: String, required: true },
+    county: { type: String },
+    postcode: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    acknowledge: { type: Boolean, required: false },
+    // cardNumber: { type: String, required: false },
+    // expiryDate: { type: String, required: false },
+    // cvv: { type: String, required: false },
+    paymentIntentID: { type: String, required: true },
+    amount: { type: Number, required: true },
+}, {
+    timestamps: true
+});
+
+orderSchema.set('toJSON', { virtuals: true, versionKey: false });
+
+orderSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('Orders', orderSchema);
